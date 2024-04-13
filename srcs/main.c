28ae9155d6108a41a9767a321427af497f1a150e
Emran <therealemran.design@gmail.com>
2024-04-13 10:47:45 +0400

    removed jank to make manual testing easier.

--- srcs/main.c
+++ srcs/main.c
@@ -4,24 +4,11 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*line;
 	t_node	*envl;
 
 	(void)argc; (void)argv;
 	envl = NULL;
-	str_arr_dup_to_list(envp, &envl);
-	while (true)
-	{
-		line = readline("falafel:~$ ");
-		ft_putendl_fd("bruh.", STDOUT_FILENO);
-		rl_on_new_line();
-		// if (!parser())
-		//		free(line), continue ;
-		// if (!interpreter())
-		//		free(line), free(tree), continue ;
-		add_history(line);
-		free(line);
-	}
+	str_arr_dup_to_list(envp, &envl); // Add check and exit fatally if it fails (it returns a bool)
 	list_destroy(&envl);
 	return (0);
 }
