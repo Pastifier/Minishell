33536e09f47dc003a634113d3a8bdb0c0d42e60b
Emran <therealemran.design@gmail.com>
2024-04-16 04:57:16 +0400

    reverted main.c to its original state

--- srcs/main.c
+++ srcs/main.c
@@ -12,73 +12,29 @@ main function calls readline and call the tokenizer and the  parser to execute c
 */
 int	main(int argc, char **argv, char **envp)
 {
-	// char 		*prompt = "$> ";
-	// char  		*line;
-	// t_astnode	*ast;
+	char 		*prompt = "$> ";
+	char  		*line;
+	t_astnode	*ast;
 	t_node		*envl;
 
-	((void)argc, (void)argv);
-	envl = NULL;
+	((void)argc, (void)argv, envl = NULL);
 	str_arr_dup_to_list(envp, &envl);
-	// while (1)
-	// {
-	// 	line = readline(prompt);
-	// 	if (line == NULL)
-	// 		break;
-	// 	if (line[0] != '\0')
-	// 	{
-	// 		add_history(line) ;
-	// 		ast = tokenize(line);
-	// 		if (ast)
-	// 		{
-	// 			interpret(ast, envl);
-	// 			destroy_ast(ast);
-	// 		}
-	// 	}
-	// 	free(line);
-	// }
-	t_astnode word, word2, word3, piper, piper2;
-	piper = (t_astnode)
+	while (1)
 	{
-		.type = TK_PIPE,
-		.parent = &piper2,
-		.left = &word,
-		.right = &word2
-	};
-	piper2 = (t_astnode)
-	{
-		.type = TK_PIPE,
-		.parent = NULL,
-		.left = &piper,
-		.right = &word3
-	};
-	word = (t_astnode)
-	{
-		.type = TK_WORD,
-		.parent = &piper,
-		.left = NULL,
-		.right = NULL,
-		.data.command.args = (char*[3]){"/bin/cat", "Makefile", NULL},
-		.data.command.thereispipe = false
-	};
-	word2 = (t_astnode)
-	{
-		.type = TK_WORD,
-		.parent = &piper,
-		.left = NULL,
-		.right = NULL,
-		.data.command.args = (char*[3]){"grep", "s", NULL},
-		.data.command.thereispipe = true
-	};
-	word3 = (t_astnode)
-	{
-		.type = TK_WORD,
-		.parent = &piper2,
-		.left = NULL,
-		.right = NULL,
-		.data.command.args = (char*[3]){"wc", "-l", NULL},
-		.data.command.thereispipe = false
-	};
-	interpret(&piper, envl);
+		line = readline(prompt);
+		if (line == NULL)
+			break;
+		if (line[0] != '\0')
+		{
+			add_history(line) ;
+			ast = tokenize(line);
+			if (ast)
+			{
+				interpret(ast, envl);
+				destroy_ast(ast);
+			}
+		}
+		free(line);
+	}
 	return (0);
 }
