8dca42fecd03162ad43eb13c32fc250dee78a45f
Emran <therealemran.design@gmail.com>
2024-04-24 12:58:59 +0400

    Changed interpret to return the exit code of the rightmost word. Now, it needs to check if the node ACTUALLY exited, or if it got signaled and whatnot.

--- includes/interpreter.h
+++ includes/interpreter.h
@@ -4,7 +4,7 @@
 # include "minishell.h"
 
 // @author Emran BinJamaan
-void	interpret(t_astnode *root, t_node *envl);
+int	interpret(t_astnode *root, t_node *envl);
 
 /*--- PREORDER FUNCTIONS ---*/
 
@@ -14,7 +14,7 @@ void	interpret(t_astnode *root, t_node *envl);
 int	prepare_rredir(t_astnode *rredir);
 
 // @author Emran BinJamaan
-int		prepare_pipenode(t_astnode *pipenode);
+int	prepare_pipenode(t_astnode *pipenode);
 
 /*--- POSTORDER FUNCTIONS ---*/
 
@@ -28,12 +28,12 @@ int		prepare_pipenode(t_astnode *pipenode);
 // created when `pipenode`'s children were handled. It doesn't close the
 // READ_END of the pipe in `pipenode`'s right (if it exists), because that will
 // be used in the pipe after it.
-int		handle_pipe(t_astnode *pipenode);
+int	handle_pipe(t_astnode *pipenode);
 // [ DEPRECATED ]
 
 // @author Emran BinJamaan
 // @brief Executes word depending on its metadata provided by the `prepare_*()` functions.
 // @return Exit code of the word should it have executed. EXIT_FATAL if
 // execution needs to stop. EXIT_SUCCESS otherwise.
-int		handle_word(t_astnode *word, t_node *envl);
+int	handle_word(t_astnode *word, t_node *envl);
 #endif // !INTERPRETER_H
