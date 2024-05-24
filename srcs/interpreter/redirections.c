3a1a1be0b3252f839f63a7d66c6329830bfd8a32
Emran <therealemran.design@gmail.com>
2024-05-24 11:40:50 +0400

    Added permission checks, but need to make sure the words don't execute upon check failure

--- srcs/interpreter/redirections.c
+++ srcs/interpreter/redirections.c
@@ -20,16 +20,25 @@ int handle_lredir(t_astnode *lredir)
 
 int handle_rredir(t_astnode *rredir)
 {
-  int fd;
+	int fd;
+	t_astnode *closest_word;
 
-  if (rredir->type != TK_RREDIR)
-    return (EXIT_NEEDED);
-  if (!access(rredir->data.redirection.filename, F_OK))
-    unlink(rredir->data.redirection.filename);
-  close(STDOUT_FILENO);
-  fd = open(rredir->data.redirection.filename, O_CREAT | O_WRONLY, 0755);
-  if (fd < 0)
-    return (EXIT_FATAL);
-  rredir->data.redirection.fd = fd;
-  return (EXIT_SUCCESS);
+	if (rredir->type != TK_RREDIR)
+		return (EXIT_NEEDED);
+	closest_word = rredir->parent;
+	if (access(rredir->data.redirection.filename, W_OK))
+	{	// stop nearest word from executing.
+		while (closest_word && closest_word->type != TK_WORD)
+			closest_word = closest_word->parent;
+		closest_word->data.command.execute = false;
+		return (perror(rredir->data.redirection.filename), EXIT_NEEDED);
+	}
+	if (!access(rredir->data.redirection.filename, F_OK))
+		unlink(rredir->data.redirection.filename);
+	close(STDOUT_FILENO);
+	fd = open(rredir->data.redirection.filename, O_CREAT | O_WRONLY, 0755);
+	if (fd < 0)
+		return (EXIT_FATAL);
+	rredir->data.redirection.fd = fd;
+	return (EXIT_SUCCESS);
 }
