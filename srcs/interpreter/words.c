df29a01bd14f18602642444aacca8c77386c7e66
Emran <therealemran.design@gmail.com>
2024-06-04 05:36:41 +0400

    Added some builtins

--- srcs/interpreter/words.c
+++ srcs/interpreter/words.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 08:29:40 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/06/03 15:42:11 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/06/04 05:33:14 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,23 @@
 
 extern int	g_signal;
 
-int	execute_word_leaf_node(t_astnode *word, t_node *envl);
+static int	execute_word_leaf_node(t_astnode *word, t_node *envl, t_shcontext *mshcontext);
+static int	execute_builtin(t_astnode *word, t_shcontext *mshcontext);
+static bool	is_builtin(t_astnode *word, t_shcontext *mshcontext);
 
-int	handle_word(t_astnode *word, t_node *envl)
+int	handle_word(t_astnode *word, t_node *envl, t_shcontext *mshcontext)
 {
 
 	if (word->type != TK_WORD)
 		return (EXIT_NEEDED);
-	execute_word_leaf_node(word, envl);
+	if (is_builtin(word, mshcontext) && !word->parent)
+		execute_builtin(word, mshcontext);
+	else
+		execute_word_leaf_node(word, envl, mshcontext);
 	return (WEXITSTATUS(word->data.command.exit));
 }
 
-int	execute_word_leaf_node(t_astnode *word, t_node *envl)
+int	execute_word_leaf_node(t_astnode *word, t_node *envl, t_shcontext *mshcontext)
 {
 	pid_t		pid;
 	char		**envp;
@@ -53,7 +58,9 @@ int	execute_word_leaf_node(t_astnode *word, t_node *envl)
 				dup2(word->data.command.fd[WRITE_END], STDOUT_FILENO);
 			close(word->data.command.fd[WRITE_END]);
 		}
-		if (word->data.command.execute)
+		if (word->data.command.builtin)
+			exit(execute_builtin(word, mshcontext));
+		else if (word->data.command.execute)
 			wexecve(word, envl, envp);
 		(str_arr_destroy(envp), list_destroy(&envl));
 		// destroy stuff.
@@ -72,4 +79,38 @@ int	execute_word_leaf_node(t_astnode *word, t_node *envl)
 		word->data.command.pid = pid;
 	}
 	return (EXIT_SUCCESS);
+}
+
+static bool	is_builtin(t_astnode *word, t_shcontext *mshcontext)
+{
+	char	*cmd;
+
+	(void)mshcontext;
+	cmd = word->data.command.args->content;
+	if (!ft_strncmp(cmd, "cd", -1)
+		|| !ft_strncmp(cmd, "env", -1)
+		|| !ft_strncmp(cmd, "pwd", -1)
+		|| !ft_strncmp(cmd, "echo", -1)
+		|| !ft_strncmp(cmd, "unset", -1)
+		|| !ft_strncmp(cmd, "export", -1)
+		|| !ft_strncmp(cmd, "exit", -1))
+	{
+		word->data.command.builtin = true;
+		return (true);
+	}
+	return (false);
+}
+
+static int	execute_builtin(t_astnode *word, t_shcontext *mshcontext)
+{
+	char	*cmd;
+
+	cmd = word->data.command.args->content;
+	if (!ft_strncmp(cmd, "cd", -1))
+		return (wcd(word, mshcontext));
+	if (!ft_strncmp(cmd, "env", -1))
+		return (env(&mshcontext->envl));
+	if (!ft_strncmp(cmd, "pwd", -1))
+		return (pwd());
+	return (EXIT_FATAL);
 }
