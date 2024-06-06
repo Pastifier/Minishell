05e444a54128f88bea208b173c56020a5bb9af9b
Emran <therealemran.design@gmail.com>
2024-06-07 00:44:26 +0400

    Welcome to minishell >:)

--- srcs/main.c
+++ srcs/main.c
@@ -5,6 +5,8 @@
 
 int	g_signal = 0;
 
+static bool init_envl(t_node **envl);
+
 int	main(int argc, char **argv, char **envp)
 {
 	char 		*prompt = "$> ";
@@ -15,14 +17,16 @@ int	main(int argc, char **argv, char **envp)
 	int			parse_ret;
 
 	((void)argc, (void)argv, envl = NULL);
+	if (!init_envl(&envl))
+		return (EXIT_FATAL);
 	str_arr_dup_to_list(envp, &envl);
 	while (true)
 	{
+		ast = NULL;
+		token_list = NULL;
 		line = readline(prompt);
 		if (line == NULL)
 			break ;
-		ast = NULL;
-		token_list = NULL;
 		if (line[0] != '\0')
 		{
 			parse_ret = init_tokenizer(line, &ast, &token_list, &envl);
@@ -39,6 +43,8 @@ int	main(int argc, char **argv, char **envp)
 				add_history(line);
 			}
 		}
+		ft_putnbr_fd(*(int*)(envl->content), 1);
+		ft_putendl_fd("", 1);
 		free(line);
 		rl_on_new_line();
 	}
@@ -46,4 +52,20 @@ int	main(int argc, char **argv, char **envp)
 	list_destroy(&envl);
 	clear_history();
 	destroy_mini_shell(&token_list, &ast, EXIT_SUCCESS);
+}
+
+static bool init_envl(t_node **envl)
+{
+	*envl = malloc(sizeof(t_node));
+	if (!*envl)
+		return (false);
+	(*envl)->content = ft_calloc(1, sizeof(int));
+	if (!(*envl)->content)
+		return (free(*envl), *envl = NULL, false);
+	*(int*)(*envl)->content = EXIT_SUCCESS;
+	(*envl)->next = NULL;
+	(*envl)->prev = NULL;
+	(*envl)->visible = false;
+	(*envl)->is_env = false;
+	return (true);
 }
