c123a2600c679cd0d632b12ab0625be4b381d39b
Emran <therealemran.design@gmail.com>
2024-06-07 00:35:40 +0400

    fixed improper usage of lazyeval

--- srcs/destroy.c
+++ srcs/destroy.c
@@ -21,7 +21,7 @@ void destroy_tokens(t_token **tokens)
 
     if (!tokens || !*tokens)
         return ;
-    while (tokens && (*tokens)->next)
+    while (*tokens && (*tokens)->next)
     {
         temp = *tokens;
         *tokens = (*tokens)->next;
