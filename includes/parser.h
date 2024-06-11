e439e7fb35e2615f0192d7d5276f34b7b4580320
Ahmed Alshafy <aalshafy@lab3r1s1.42abudhabi.ae>
2024-06-11 13:44:47 +0400

    merge parse redir on one func

--- includes/parser.h
+++ includes/parser.h
@@ -34,8 +34,8 @@ int   parse(t_token **tokens_iter, t_astnode **node, t_node **envl);
 int parse_word(t_token **token_list, t_astnode **node);
 int parse_env(t_token **token_list, t_astnode **node, t_node **envl);
 int parse_pipe(t_token **token_list, t_astnode **node);
-int parse_rredir(t_token **token_list, t_astnode **node);
-int parse_lredir(t_token **token_list, t_astnode **node);
+// int parse_rredir(t_token **token_list, t_astnode **node);
+int parse_redir(t_token **token_list, t_astnode **node);
 int parse_lappend(t_token **token_list, t_astnode **node);
 int parse_rappend(t_token **token_list, t_astnode **node);
 
--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -17,17 +17,15 @@ int   parse(t_token **tokens_iter, t_astnode **node, t_node **envl)
         ret = parse_pipe(tokens_iter, node);
     else if ((*tokens_iter)->token_type == TK_LREDIR 
         || (*tokens_iter)->token_type == TK_LAPPEND)
-        ret = parse_lredir(tokens_iter, node);
+        ret = parse_redir(tokens_iter, node);
     else if ((*tokens_iter)->token_type == TK_RREDIR
         || (*tokens_iter)->token_type == TK_RAPPEND)
-        ret = parse_rredir(tokens_iter, node);
+        ret = parse_redir(tokens_iter, node);
     if (ret)
         return (ret);
     if (*tokens_iter && (*tokens_iter)->next != NULL)
     {
         *tokens_iter = (*tokens_iter)->next;
-        // printf("token type: %d - token value: %s\n", (*tokens_iter)->token_type, (*tokens_iter)->value);
-        // printf("node type: %d\n", (*node)->type);
         return (parse(tokens_iter, node, envl));
     }
     return (0);
@@ -67,42 +65,42 @@ int parse_pipe(t_token **token_list, t_astnode **node)
     return (0);
 }
 
-int parse_rredir(t_token **token_list, t_astnode **node) // can be the same as parse_lredir
-{
-    t_astnode *new_node;
-    t_astnode *iter;
+// int parse_rredir(t_token **token_list, t_astnode **node) // can be the same as parse_lredir
+// {
+//     t_astnode *new_node;
+//     t_astnode *iter;
 
-    if (((*token_list)->next && (*token_list)->next->token_type != TK_WORD) || !(*token_list)->next)    
-        return (2);
-    new_node = (t_astnode *)malloc(sizeof(t_astnode));
-    if (new_node == NULL)
-        return (1);
-    if ((*token_list)->token_type == TK_RAPPEND)
-        new_node->data.redirection.mode = O_APPEND;
-    else
-        new_node->data.redirection.mode = 0;
-    new_node->type = TK_RREDIR;
-    new_node->right = NULL;
-    new_node->left = NULL;
-    new_node->data.redirection.filename = ft_strdup((*token_list)->next->value);
-    if (!(*node))
-    {
-        (*node) = new_node;
-        new_node->parent = NULL;
-    }
-    else
-    {    
-        iter = *node;
-        while (iter && iter->right)
-            iter = iter->right;
-        new_node->parent = iter;
-        iter->right = new_node;
-    }
-    *token_list = (*token_list)->next;
-    return (0);
-}
+//     if (((*token_list)->next && (*token_list)->next->token_type != TK_WORD) || !(*token_list)->next)    
+//         return (2);
+//     new_node = (t_astnode *)malloc(sizeof(t_astnode));
+//     if (new_node == NULL)
+//         return (1);
+//     if ((*token_list)->token_type == TK_RAPPEND)
+//         new_node->data.redirection.mode = O_APPEND;
+//     else
+//         new_node->data.redirection.mode = 0;
+//     new_node->type = TK_RREDIR;
+//     new_node->right = NULL;
+//     new_node->left = NULL;
+//     new_node->data.redirection.filename = ft_strdup((*token_list)->next->value);
+//     if (!(*node))
+//     {
+//         (*node) = new_node;
+//         new_node->parent = NULL;
+//     }
+//     else
+//     {    
+//         iter = *node;
+//         while (iter && iter->right)
+//             iter = iter->right;
+//         new_node->parent = iter;
+//         iter->right = new_node;
+//     }
+//     *token_list = (*token_list)->next;
+//     return (0);
+// }
 
-int parse_lredir(t_token **token_list, t_astnode **node)
+int parse_redir(t_token **token_list, t_astnode **node)
 {
     t_astnode *new_node;
     t_astnode *iter;
@@ -112,11 +110,12 @@ int parse_lredir(t_token **token_list, t_astnode **node)
     new_node = (t_astnode *)malloc(sizeof(t_astnode));
     if (new_node == NULL)
         return (1);
-    if ((*token_list)->token_type == TK_LAPPEND)
+    if ((*token_list)->token_type == TK_LAPPEND 
+        || (*token_list)->token_type == TK_RAPPEND)
         new_node->data.redirection.mode = O_APPEND;
     else
         new_node->data.redirection.mode = 0;
-    new_node->type = TK_LREDIR;
+    new_node->type = (*token_list)->token_type;
     if (!(*node))
     {
         (*node) = new_node;
