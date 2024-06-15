c6bf724c647bfbcf2951ee16aa810efbec192c90
Pastifier <136130975+Pastifier@users.noreply.github.com>
2024-06-15 04:30:07 +0400

    Update interpreter.c

--- srcs/interpreter/interpreter.c
+++ srcs/interpreter/interpreter.c
@@ -12,7 +12,7 @@
 
 #include "interpreter.h"
 
-extern volatile __sig_atomic_t sig = 0;
+extern volatile __sig_atomic_t	sig;
 
 static t_shcontext	init_context(t_astnode *root, t_node *envl);
 static void			visit(t_astnode *node, t_node *envl,
