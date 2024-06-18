48d9cf4e29ac6858c05efefc0853bdff0a041e85
Emran <therealemran.design@gmail.com>
2024-06-18 09:56:41 +0400

    Norminette.

--- includes/interpreter.h
+++ includes/interpreter.h
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   interpreter.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 09:53:03 by ebinjama          #+#    #+#             */
+/*   Updated: 2024/06/18 09:56:12 by ebinjama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INTERPRETER_H
 # define INTERPRETER_H
 
 # include "minishell.h"
 # include <signal.h>
 
-#ifdef __linux__
-# define EXIT_INVAL_ARG		2
-#endif // !__LINUX__
+# ifdef __linux__
+#  define EXIT_INVAL_ARG		2
+# endif // !__LINUX__
 
-#ifdef __MACH__
-# define EXIT_INVAL_ARG		255
-#endif // !__MACH__
+# ifdef __MACH__
+#  define EXIT_INVAL_ARG		255
+# endif // !__MACH__
 
 typedef struct sigaction	t_sigaction;
 typedef struct s_tree
@@ -48,12 +60,13 @@ int		prepare_heredoc(t_astnode *lredir, t_shcontext *mshcontext);
 /*--- POSTORDER FUNCTIONS ---*/
 
 // @author Emran BinJamaan
-// @brief	Resets the standard file-descriptors in order to emulate bash behaviour
-//			more accurately.
+// @brief	Resets the standard file-descriptors in order to
+//			emulate bash behaviour more accurately.
 int		handle_pipe(t_astnode *pipenode, t_shcontext *mshcontext);
 
 // @author Emran BinJamaan
-// @brief Executes word depending on its metadata provided by the `prepare_*()` functions.
+// @brief Executes word depending on its metadata provided by the
+//		 `prepare_*()` functions.
 // @return Exit code of the word should it have executed. EXIT_FATAL if
 // execution needs to stop. EXIT_SUCCESS otherwise.
 int		handle_word(t_astnode *word, t_node *envl, t_shcontext *mshcontext);
@@ -62,10 +75,10 @@ int		handle_word(t_astnode *word, t_node *envl, t_shcontext *mshcontext);
 // @brief	Handles left redirections.
 // @warning	This function assumes that the AST is structured perfectly,
 //			i.e, input redirections are children to words.
-int 	handle_lredir(t_astnode *lredir, t_shcontext *mshcontext);
+int		handle_lredir(t_astnode *lredir, t_shcontext *mshcontext);
 
 // @author Emran BinJamaan
-int 	handle_rredir(t_astnode *rredir, t_shcontext *mshcontext);
+int		handle_rredir(t_astnode *rredir, t_shcontext *mshcontext);
 
 // @author Emran BinJamaan
 int		handle_heredoc(t_astnode *heredoc, t_shcontext *mshcontext);
@@ -76,11 +89,11 @@ int		wcd(t_astnode *cdnode, t_shcontext *mshcontext);
 
 // @author Ahmed Alshafy
 // @brief  wrapper for the `export` built-in.
-int		parse_export(const char *var_name); // I told you to make it const, bro :(
+int		parse_export(const char *var_name);
 
 // @author Emran BinJamaan
 int		wexport(t_astnode *word, t_node **envp, const char *variable,
-	const char *value);
+			const char *value);
 
 // @author Emran BinJamaan
 int		wunset(t_astnode *word, t_node **envp);
