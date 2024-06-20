d4f780532c9192deafa2465c3b05992cc0bc85be
Emran Binjamaan <ebinjama@lab3r8s4.42abudhabi.ae>
2024-06-20 19:46:38 +0400

    Added detailed comment about the helper function to add to

--- srcs/builtins/builtins_cd.c
+++ srcs/builtins/builtins_cd.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 10:09:10 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/06/20 18:17:37 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/06/20 19:45:33 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,25 @@ static int	cd(char *path, t_node **envp);
 // TODO:
 // Apparently, '~' expansion and '-' should be handled...
 
+// Notes:
+// Tilde expansion only works when the path starts with a tilde
+
 int	wcd(t_astnode *cdnode, t_shcontext *mshcontext)
 {
 	t_node	*cdarg;
+	t_node	*home;
+	t_node	*oldpwd;
 
 	cdarg = cdnode->data.command.args->next;
+	home = find_variable(&mshcontext->envl, "HOME=");
+	oldpwd = find_variable(&mshcontext->envl, "OLDPWD=");
+	// Add a function which handles both. It will check whether the line starts with a tilde
+	// or a hyphen, and acts appropriately. It will return an error code should it have found
+	// a tilde at the start and didn't find HOME, same with OLDPWD and hyphen.
+	// It will return an error code should it have found a hyphen at the start, but with
+	// characters after it (e.g. `cd -hello` outputs "-h not a known option").
+	// Otherwise, it will just modify the cdarg's content appropriately, and let the rest
+	// of this function do its thing.
 	if (cdarg && OS_IS_MAC)
 		return (cd(cdarg->content, &(mshcontext->envl)));
 	if (cdarg && cdarg->next)
